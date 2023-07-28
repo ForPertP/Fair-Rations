9812dedce47b957b86a57820fd3081f868b80e6b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-07-28 18:46:33 +0900

    Update Fair-Rations.cs

--- Fair-Rations.cs
+++ Fair-Rations.cs
@@ -27,3 +27,24 @@ class Result
     }
 
 }
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> B = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(BTemp => Convert.ToInt32(BTemp)).ToList();
+
+        string result = Result.fairRations(B);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
